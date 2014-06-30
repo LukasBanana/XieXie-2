9d6c352bebc5c9b1b19fe46a8fa1041aebeb43c1
Lukas Hermanns <lhlukash23@googlemail.com>
2014-06-30 14:43:56 +0200

    - Added CodeBlock AST class.

---
+++ sources/Compiler/AbstractSyntaxTrees/CodeBlock.h
@@ -0,0 +1,47 @@
+/*
+ * Code block AST header
+ * 
+ * This file is part of the "XieXie-Compiler" (Copyright (c) 2013 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef __XX_AST_CODE_BLOCK_H__
+#define __XX_AST_CODE_BLOCK_H__
+
+
+#include "StmntList.h"
+
+
+namespace AbstractSyntaxTrees
+{
+
+
+class CodeBlock : public AST
+{
+    
+    public:
+        
+        DefineASTVisitProc(CodeBlock);
+
+        void RefreshSourceArea()
+        {
+            if (stmntList)
+            {
+                stmntList->RefreshSourceArea();
+                sourceArea = stmntList->sourceArea;
+            }
+        }
+
+        StmntListPtr stmntList;
+
+};
+
+
+} // /namespace SyntacticAnalyzer
+
+
+#endif
+
+
+
+// ================================================================================
