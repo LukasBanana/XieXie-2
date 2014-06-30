da2797b5afd88d65d7d4b7d08e1b70152c4e5384
Lukas Hermanns <lhlukash23@googlemail.com>
2014-06-30 14:13:53 +0200

    - Added SourceArea class.

---
+++ sources/Compiler/SyntaxAnalyzer/SourceArea.cpp
@@ -0,0 +1,39 @@
+/*
+ * Source area file
+ * 
+ * This file is part of the "XieXie-Compiler" (Copyright (c) 2013 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#include "SourceArea.h"
+#include "StringMod.h"
+
+
+namespace SyntacticAnalyzer
+{
+
+
+const SourceArea SourceArea::ignore;
+
+SourceArea::SourceArea(const SourcePosition& startPos, const SourcePosition& endPos) :
+    start( startPos ),
+    end  ( endPops  )
+{
+}
+
+std::string SourceArea::ToString() const
+{
+    return start == end ? start.ToString() : start.ToString() + " - " + end.ToString();
+}
+
+bool SourceArea::IsValid() const
+{
+    return start <= end;
+}
+
+
+} // /namespace SyntacticAnalyzer
+
+
+
+// ================================================================================
---
+++ sources/Compiler/SyntaxAnalyzer/SourceArea.h
@@ -0,0 +1,56 @@
+/*
+ * Source area header
+ * 
+ * This file is part of the "XieXie-Compiler" (Copyright (c) 2013 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef __XX_SOURCE_AREA_H__
+#define __XX_SOURCE_AREA_H__
+
+
+#include "SourcePosition.h"
+
+
+namespace SyntacticAnalyzer
+{
+
+
+/**
+This class stores a start- and an end source position.
+\see SourcePosition
+*/
+class SourceArea
+{
+    
+    public:
+        
+        SourceArea() = default;
+        SourceArea(const SourcePosition& startPos, const SourcePosition& endPos);
+
+        /**
+        Returns the source area as string in the format "StartRow:StartColumn - EndRow:EndColumn",
+        e.g. "75:10 - 75:15" or in the format "Row:Column" if start- and end position are equal.
+        */
+        std::string ToString() const;
+
+        //! Returns ture if this is a valid source position. False if start-position > end-position.
+        bool IsValid() const;
+
+        //! Start- and end source positions.
+        SourcePosition start, end;
+
+        //! Invalid source area.
+        static const SourceArea ignore;
+
+};
+
+
+} // /namespace SyntacticAnalyzer
+
+
+#endif
+
+
+
+// ================================================================================
