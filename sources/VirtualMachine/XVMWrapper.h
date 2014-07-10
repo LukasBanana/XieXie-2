5d48404c3a621699b140d18bc8f88a7ac0b3394f
Lukas Hermanns <lhlukash23@googlemail.com>
2014-07-10 22:45:41 +0200

    - Continued with XVM C++ wrapper.

--- sources/VirtualMachine/XVMWrapper.h
+++ sources/VirtualMachine/XVMWrapper.h
@@ -9,6 +9,9 @@
 #define __XX_XVM_WRAPPER_H__
 
 
+#include <string>
+
+#define _REMOVE_XVM_TESTSUITE_
 #include "xvm.c"
 
 
@@ -25,6 +28,7 @@ class VirtualMachine
         //! Exit codes of an executed program.
         enum class ExitCodes
         {
+            __Unknown__,
             Success,
             InvalidByteCode,
             InvalidStack,
@@ -43,11 +47,24 @@ class VirtualMachine
     
             public:
         
-                ByteCode();
-                ~ByteCode();
+                ByteCode()
+                {
+                    xvm_bytecode_init(&byteCode_);
+                }
+                ~ByteCode()
+                {
+                    xvm_bytecode_free(&byteCode_);
+                }
 
-                bool ReadFromFile(const std::string& filename);
-                bool WriteToFile(const std::string& filename);
+                bool ReadFromFile(const std::string& filename)
+                {
+                    return xvm_bytecode_read_from_file(&byteCode_, filename.c_str()) != 0;
+                }
+
+                bool WriteToFile(const std::string& filename)
+                {
+                    return xvm_bytecode_write_to_file(&byteCode_, filename.c_str()) != 0;
+                }
 
             private:
         
@@ -65,23 +82,86 @@ class VirtualMachine
                 
                 static const size_t defaultSize = 256;
 
-                Stack(size_t size = defaultSize);
-                ~Stack();
+                Stack(size_t size = defaultSize)
+                {
+                    xvm_stack_init(&stack_);
+                    xvm_stack_create(&stack_, size);
+                }
+                ~Stack()
+                {
+                    xvm_stack_free(&stack_);
+                }
 
-                size_t Size() const;
+                //! Returns the number of WORD (32-bit) entries in the stack.
+                size_t Size() const
+                {
+                    return stack_.stack_size;
+                }
 
             private:
                 
+                friend class VirtualMachine;
+
                 xvm_stack stack_;
 
         };
 
         /**
         Executes the specified program using the specified stack.
-        \return Exit code. 0 on success.
         \see ExitCodes
         */
-        ExitCodes ExecuteProgram(const ByteCode& byteCode, Stack& stack);
+        ExitCodes ExecuteProgram(const ByteCode& byteCode, Stack& stack)
+        {
+            const xvm_exit_codes code = xvm_execute_program(
+                &(byteCode.byteCode_),
+                &(stack.stack_)
+            );
+            return VirtualMachine::MapExitCode(code);
+        }
+
+        /**
+        Executes the specified program from the specified entry point using the specified stack.
+        \see ExitCodes
+        */
+        ExitCodes ExecuteProgram(const ByteCode& byteCode, Stack& stack, const std::string& entryPoint)
+        {
+            const xvm_exit_codes code = xvm_execute_program_entry_point(
+                &(byteCode.byteCode_),
+                &(stack.stack_),
+                entryPoint.c_str()
+            );
+            return VirtualMachine::MapExitCode(code);
+        }
+
+    private:
+        
+        static ExitCodes MapExitCode(xvm_exit_codes code)
+        {
+            switch (code)
+            {
+                case EXITCODE_SUCCESS:
+                    return ExitCodes::Success;
+                case EXITCODE_INVALID_BYTECODE:
+                    return ExitCodes::InvalidByteCode;
+                case EXITCODE_INVALID_STACK:
+                    return ExitCodes::InvalidStack;
+                case EXITCODE_INVALID_OPCODE:
+                    return ExitCodes::InvalidOpCode;
+                case EXITCODE_INVALID_INTRINSIC:
+                    return ExitCodes::InvalidIntrinsic;
+                case EXITCODE_STACK_OVERFLOW:
+                    return ExitCodes::StackOverflow;
+                case EXITCODE_STACK_UNDERFLOW:
+                    return ExitCodes::StackUnderflow;
+                case EXITCODE_DIVISION_BY_ZERO:
+                    return ExitCodes::DivisionByZero;
+                case EXITCODE_UNKNOWN_ENTRY_POINT:
+                    return ExitCodes::UnknwonEntryPoint;
+                /*case EXITCODE_MEMORY_VIOLATION:
+                    return ExitCodes::MemoryViolation;*/
+            }
+            return ExitCodes::__Unknown__;
+        }
 
 };
 
