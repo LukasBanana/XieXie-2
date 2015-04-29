cd6f492898c193def733251522bda77ac52dcf4a
Lukas Hermanns <lhlukash23@googlemail.com>
2015-04-29 21:04:28 +0200

    Added "Math" built-in class to code generation.

--- sources/Compiler/ContextAnalyzer/StdCodeFactory.cpp
+++ sources/Compiler/ContextAnalyzer/StdCodeFactory.cpp
@@ -9,6 +9,8 @@
 #include "MakeUnique.h"
 #include "ProcDeclStmnt.h"
 #include "ProcSignature.h"
+#include "VarDecl.h"
+#include "VarDeclStmnt.h"
 #include "Param.h"
 #include "LiteralExpr.h"
 #include "BuiltinTypeDenoter.h"
@@ -64,6 +66,16 @@ static BuiltinTypeDenoterPtr Float()
     return GenBuiltinType(BuiltinTypeDenoter::TypeNames::Float);
 }
 
+// Generate automatic type AST
+static BuiltinTypeDenoterPtr AutoType(bool isConst = false)
+{
+    auto ast = GenBuiltinType(BuiltinTypeDenoter::TypeNames::AutoType);
+    {
+        ast->isConst = isConst;
+    }
+    return ast;
+}
+
 // Generate pointer type-denoter AST
 static PointerTypeDenoterPtr GenPointerType(const std::string& ident, bool isWeakRef = false)
 {
@@ -94,17 +106,21 @@ static PointerTypeDenoterPtr String(bool isWeakRef = false)
 
 static AttribPtr GenAttrib(const std::string& ident)
 {
-    auto attrib = std::make_shared<Attrib>();
-    attrib->ident = ident;
-    return attrib;
+    auto ast = std::make_shared<Attrib>();
+    {
+        ast->ident = ident;
+    }
+    return ast;
 }
 
 static AttribPrefixPtr GenAttribPrefix(const std::initializer_list<std::string>& attribIdents)
 {
-    auto attribPrefix = std::make_shared<AttribPrefix>();
-    for (const auto& ident : attribIdents)
-        attribPrefix->attribs.push_back(GenAttrib(ident));
-    return attribPrefix;
+    auto ast = std::make_shared<AttribPrefix>();
+    {
+        for (const auto& ident : attribIdents)
+            ast->attribs.push_back(GenAttrib(ident));
+    }
+    return ast;
 }
 
 // Generates the attribute prefix: [[final]]
@@ -179,12 +195,23 @@ static VarNamePtr GenVarNameList(const std::vector<std::string>& idents)
 }
 
 // Generate literal expression AST
-static ExprPtr GenExpr(int value)
+static ExprPtr GenIntExpr(std::string&& value)
 {
     auto ast = std::make_shared<LiteralExpr>();
     {
         ast->typeDenoter    = Int();
-        ast->value          = std::to_string(value);
+        ast->value          = std::move(value);
+    }
+    return ast;
+}
+
+// Generate literal expression AST
+static ExprPtr GenFloatExpr(std::string&& value)
+{
+    auto ast = std::make_shared<LiteralExpr>();
+    {
+        ast->typeDenoter    = Float();
+        ast->value          = std::move(value);
     }
     return ast;
 }
@@ -223,8 +250,8 @@ static ProcSignaturePtr GenProcSignature(const TypeDenoterPtr& returnType, const
     return ast;
 }
 
-//! Generate procedure AST
-static ProcDeclStmntPtr GenProcDecl(
+// Generate procedure declaration statement AST
+static ProcDeclStmntPtr GenProcDeclStmnt(
     ClassDeclStmnt& classDecl, const TypeDenoterPtr& returnType, const std::string& ident,
     const ParamList& params = ParamList(), const AttribPrefixPtr& attribPrefix = nullptr)
 {
@@ -237,12 +264,38 @@ static ProcDeclStmntPtr GenProcDecl(
     return ast;
 }
 
+// Generate variable declaration AST
+static VarDeclPtr GenVarDecl(VarDeclStmnt& varDeclStmnt, const std::string& ident, const ExprPtr& initExpr = nullptr)
+{
+    auto ast = std::make_shared<VarDecl>();
+    {
+        ast->ident      = ident;
+        ast->initExpr   = initExpr;
+        ast->parentRef  = &varDeclStmnt;
+    }
+    return ast;
+}
+
+// Generate variable declaration statement AST
+static VarDeclStmntPtr GenVarDeclStmnt(
+    ClassDeclStmnt& classDecl, const TypeDenoterPtr& varType,
+    const std::string& ident, const ExprPtr& initExpr = nullptr)
+{
+    auto ast = std::make_shared<VarDeclStmnt>();
+    {
+        ast->typeDenoter    = varType;
+        ast->parentRef      = &classDecl;
+        ast->varDecls.push_back(GenVarDecl(*ast, ident, initExpr));
+    }
+    return ast;
+}
+
 // Generate member procedure AST
 static void GenMemberProc(
     ClassDeclStmnt& classDecl, const TypeDenoterPtr& returnType, const std::string& ident,
     const ParamList& params = ParamList(), const AttribPrefixPtr& attribPrefix = nullptr)
 {
-    auto ast = GenProcDecl(classDecl, returnType, ident, params, attribPrefix);
+    auto ast = GenProcDeclStmnt(classDecl, returnType, ident, params, attribPrefix);
     classDecl.declStmnts.push_back(ast);
 }
 
@@ -251,8 +304,16 @@ static void GenStaticProc(
     ClassDeclStmnt& classDecl, const TypeDenoterPtr& returnType, const std::string& ident,
     const ParamList& params = ParamList(), const AttribPrefixPtr& attribPrefix = nullptr)
 {
-    auto ast = GenProcDecl(classDecl, returnType, ident, params, attribPrefix);
+    auto ast = GenProcDeclStmnt(classDecl, returnType, ident, params, attribPrefix);
     ast->procSignature->isStatic = true;
+    classDecl.declStmnts.push_back(ast);
+}
+
+// Generate static constant AST
+static void GenStaticConst(ClassDeclStmnt& classDecl, const std::string& ident, const ExprPtr& initExpr)
+{
+    auto ast = GenVarDeclStmnt(classDecl, AutoType(true), ident, initExpr);
+    ast->isStatic = true;
     classDecl.declStmnts.push_back(ast);
 }
 
@@ -281,13 +342,14 @@ static void GenReleaseProc(ClassDeclStmnt& classDecl)
 }
 
 // Generate class declaration statement AST
-static std::unique_ptr<ClassDeclStmnt> GenClass(const std::string& ident)
+static std::unique_ptr<ClassDeclStmnt> GenClass(const std::string& ident, const AttribPrefixPtr& attribPrfix = nullptr)
 {
     auto ast = MakeUnique<ClassDeclStmnt>();
     {
-        ast->isBuiltin  = true;
-        ast->isExtern   = true;
-        ast->ident      = ident;
+        ast->isBuiltin      = true;
+        ast->isExtern       = true;
+        ast->ident          = ident;
+        ast->attribPrefix   = attribPrfix;
     }
     return ast;
 }
@@ -364,7 +426,7 @@ static std::unique_ptr<ClassDeclStmnt> GenStringClass()
     GenMemberProc(*ast, String(), "append", GenParam(Bool(), "rhs"));
     GenMemberProc(*ast, String(), "append", GenParam(Int(), "rhs"));
     GenMemberProc(*ast, String(), "append", GenParam(Float(), "rhs"));
-    GenMemberProc(*ast, String(), "subString", ( GenParam(Int(), "pos"), GenParam(Int(), "len", GenExpr(-1)) ));
+    GenMemberProc(*ast, String(), "subString", ( GenParam(Int(), "pos"), GenParam(Int(), "len", GenIntExpr("-1")) ));
     GenMemberProc(*ast, Void(), "setChar", ( GenParam(Int(), "pos"), GenParam(Int(), "char") ));
     GenMemberProc(*ast, Int(), "getChar", GenParam(Int(), "pos"));
     GenMemberProc(*ast, Int(), "pointer", ParamList(), AttrOverrideFinal());
@@ -420,7 +482,7 @@ static std::unique_ptr<ClassDeclStmnt> GenBufferClass()
 
 static std::unique_ptr<ClassDeclStmnt> GenIntrinsicsClass()
 {
-    auto ast = GenClass("Intrinsics");
+    auto ast = GenClass("Intrinsics", AttrFinal());
 
     auto voidType   = Void();
     auto intType    = Int();
@@ -455,6 +517,21 @@ static std::unique_ptr<ClassDeclStmnt> GenIntrinsicsClass()
     GenStaticProc(*ast, floatType, "readFloat", GenParam(intType, "handlePtr"));
     GenStaticProc(*ast, voidType, "readBuffer", ( GenParam(intType, "handlePtr"), GenParam(intType, "bufferPtr"), GenParam(intType, "size") ));
 
+    GenStaticProc(*ast, intType, "time");
+    GenStaticProc(*ast, voidType, "sleep", GenParam(intType, "duration"));
+
+    return ast;
+}
+
+static std::unique_ptr<ClassDeclStmnt> GenMathClass()
+{
+    auto ast = GenClass("Math", AttrFinal());
+
+    auto floatType = Float();
+
+    GenStaticConst(*ast, "pi", GenFloatExpr("3.14159265358979"));
+    GenStaticConst(*ast, "e", GenFloatExpr("2.71828182845905"));
+
     GenStaticProc(*ast, floatType, "sin", GenParam(floatType, "x"));
     GenStaticProc(*ast, floatType, "cos", GenParam(floatType, "x"));
     GenStaticProc(*ast, floatType, "tan", GenParam(floatType, "x"));
@@ -467,10 +544,8 @@ static std::unique_ptr<ClassDeclStmnt> GenIntrinsicsClass()
     GenStaticProc(*ast, floatType, "log10", GenParam(floatType, "x"));
     GenStaticProc(*ast, floatType, "sqrt", GenParam(floatType, "x"));
 
-    GenStaticProc(*ast, intType, "randInt");
+    GenStaticProc(*ast, Int(), "randInt");
     GenStaticProc(*ast, floatType, "randFloat");
-    GenStaticProc(*ast, intType, "time");
-    GenStaticProc(*ast, voidType, "sleep", GenParam(intType, "duration"));
 
     return ast;
 }
@@ -503,13 +578,14 @@ static void GenSingleStmntAndAppendToClass(ClassDeclStmnt& classDecl, const Proc
 
 void GenerateBuiltinClasses(std::vector<std::unique_ptr<ClassDeclStmnt>>& classDeclList)
 {
-    classDeclList.push_back(GenObjectClass());
-    classDeclList.push_back(GenStringClass());
-    classDeclList.push_back(GenArrayClass());
-    classDeclList.push_back(GenPrimArrayClass());
-    classDeclList.push_back(GenBoolArrayClass());
-    classDeclList.push_back(GenBufferClass());
-    classDeclList.push_back(GenIntrinsicsClass());
+    classDeclList.push_back( GenObjectClass()     );
+    classDeclList.push_back( GenStringClass()     );
+    classDeclList.push_back( GenArrayClass()      );
+    classDeclList.push_back( GenPrimArrayClass()  );
+    classDeclList.push_back( GenBoolArrayClass()  );
+    classDeclList.push_back( GenBufferClass()     );
+    classDeclList.push_back( GenIntrinsicsClass() );
+    classDeclList.push_back( GenMathClass()       );
 }
 
 void GenerateBuiltinClasses(AbstractSyntaxTrees::Program& program)
@@ -535,7 +611,7 @@ void GenerateSetter(const VarDecl& memberVar, const VarDecl::Vis visibility)
 
     /* Generate procedure signature */
     auto param = GenParam(memberVar.GetTypeDenoter()->CopyRef(), varIdent);
-    auto procDecl = GenProcDecl(classDecl, Void(), procIdent, param, AttrFinal());
+    auto procDecl = GenProcDeclStmnt(classDecl, Void(), procIdent, param, AttrFinal());
 
     procDecl->procSignature->isStatic = memberVar.IsStatic();
 
@@ -566,7 +642,7 @@ void GenerateGetter(const VarDecl& memberVar, const VarDecl::Vis visibility)
 
     /* Generate procedure signature */
     auto returnType = memberVar.GetTypeDenoter()->CopyRef();
-    auto procDecl = GenProcDecl(classDecl, returnType, procIdent, ParamList(), AttrFinal());
+    auto procDecl = GenProcDeclStmnt(classDecl, returnType, procIdent, ParamList(), AttrFinal());
 
     procDecl->procSignature->isStatic = memberVar.IsStatic();
     
