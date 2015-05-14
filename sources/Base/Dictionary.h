77e3fdeba259180395d816b7bbd21385a25d31b6
Lukas Hermanns <lhlukash23@googlemail.com>
2015-05-14 11:49:54 +0200

    Added documentation to the "Dictionary" template class.

--- sources/Base/Dictionary.h
+++ sources/Base/Dictionary.h
@@ -13,17 +13,22 @@
 #include <map>
 
 
-//! Class for dictionaries with string key and pointer value.
+/**
+Class for dictionaries with string key and pointer value.
+\tparam T Specifies the non-pointer value type.
+*/
 template <typename T> class Dictionary final
 {
     
     public:
         
+        //! Registers the specified pointer entry with the unique identifier 'ident'.
         void Register(const std::string& ident, T* entry)
         {
             map_[ident] = entry;
         }
 
+        //! Unregisters a pointer entry with the specified identifier 'ident'.
         T* Unregister(const std::string& ident)
         {
             auto it = map_.find(ident);
@@ -36,16 +41,22 @@ template <typename T> class Dictionary final
             return nullptr;
         }
 
+        //! Clears the entire dictionary map.
         void Clear()
         {
             map_.clear();
         }
 
+        //! Returns true if the specified pointer entry is contained in this dictionary.
         bool Has(T* entry) const
         {
             return map_.find(entry) != map_.end();
         }
 
+        /**
+        Returns a pointer to the entry with the specified registered identifier 'ident',
+        or null if there is no such entry.
+        */
         T* Find(const std::string& ident) const
         {
             auto it = map_.find(ident);
